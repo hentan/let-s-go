f76a08814b402589bf77c560b592b176877cec7d
Расходчиков Михаил <37670426+hentan@users.noreply.github.com>
2024-04-12 16:55:53 +0300

    lesson 10 упражнение 1-3

---
+++ lesson_10_tasks/lesson_10.2/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "automacon_cource_go/let-s-go/lesson_10_tasks/lesson_10.1/first"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("ничего не делаем")
+}
---
+++ lesson_10_tasks/lesson_10.3/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"automacon_cource_go/let-s-go/lesson_10_tasks/lesson_10.1/first"
+	"automacon_cource_go/let-s-go/lesson_10_tasks/lesson_10.3/second"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(first.Hello())
+	fmt.Println(second.Hello())
+}
